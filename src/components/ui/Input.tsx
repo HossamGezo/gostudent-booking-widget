f49039f57c49acb8020ca3413a146faaa16b491f
HossamGezo <ha2ghossam10@gmail.com>
2026-06-15 05:13:07 +0300

    style: update base UI components styling for wordpress compatibility

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -5,7 +5,7 @@ const Input = ({ className, ref, ...props }: React.ComponentProps<"input">) => {
     <input
       ref={ref}
       className={cn(
-        "bg-background-overview border-border-default focus:border-border-focus placeholder:text-text-secondary text-text-primary h-10.5 w-full rounded-[3px] border px-2.5 text-[10px] outline-hidden transition-colors placeholder:text-[10px]",
+        "bg-background-overview border-border-default focus:border-border-focus placeholder:text-text-primary/60 text-text-primary h-10.5 w-full rounded-[3px] border px-2.5 text-[10px] transition-colors placeholder:text-[10px]",
         className,
       )}
       {...props}
